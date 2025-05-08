8cb0baf4bcd43720c6be8d245d609ff825559312
Kacper Chętkowski <kacper.chetkowski@gmail.com>
2025-05-08 21:19:36 +0200

    Router hash

--- src/App.tsx
+++ src/App.tsx
@@ -2,53 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {HashRouter, Link, Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-      <BrowserRouter>
-          <main className="App">
+    return (
+        <HashRouter>
             <Routes>
-              <Route path="/" element={
-                <div className="App">
-                  <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                      Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                      Learn React
-                    </a>
-                  </header>
-                </div>
-              }/>
-              <Route path="/foo" element={
-                <div className="App">
-                  <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                      Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                      Foo
-                    </a>
-                  </header>
-                </div>
-              }/>
+                <Route path="/" element={
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <Link to="/foo">Foo</Link>
+                        </header>
+                    </div>
+                }/>
+                <Route path="/foo" element={
+                    <div className="App">
+                        <header className="App-header">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <p>
+                                Edit <code>src/App.tsx</code> and save to reload.
+                            </p>
+                            <Link to="/">Home</Link>
+                        </header>
+                    </div>
+                }/>
             </Routes>
-          </main>
-      </BrowserRouter>
-  );
+        </HashRouter>
+    );
 }
 
 export default App;
